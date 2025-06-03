0bd3dd59cf7cf15041a036373181f5f19e02d847
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-06-03 16:53:39 -0500

    feat: :sparkles: Se completó el punto 2 de multitablas

--- Consultas/consultas_multitablas.sql
+++ Consultas/consultas_multitablas.sql
@@ -6,3 +6,7 @@ SHOW TABLES;
 SELECT nombre AS Cliente, detalle_id, pedido_id, cantidad, precio_unitario  FROM usuarios AS u
 JOIN detalles_pedidos AS d ON u.usuario_id = d.pedido_id
 WHERE u.tipo_id = 1;
+
+-- 2
+SELECT nombre, precio_unitario FROM detalles_pedidos AS dp
+JOIN productos AS p ON dp.producto_id = p.producto_id
