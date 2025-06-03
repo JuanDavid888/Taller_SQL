3bd066ae520b60d4a5d012334d5797b8bca3fe9f
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-06-03 16:47:19 -0500

    feat: :sparkles: Se completó el punto 1 de multitabla

---
+++ Consultas/consultas_multitablas.sql
@@ -0,0 +1,8 @@
+-- Active: 1748975579213@@127.0.0.1@3307@taller_sql
+
+SHOW TABLES;
+
+-- 1
+SELECT nombre AS Cliente, detalle_id, pedido_id, cantidad, precio_unitario  FROM usuarios AS u
+JOIN detalles_pedidos AS d ON u.usuario_id = d.pedido_id
+WHERE u.tipo_id = 1;
