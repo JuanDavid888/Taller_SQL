ed4ac0ffea6c07481881c43eaed36a3ce3c5f059
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-06-03 17:25:09 -0500

    feat: :sparkles: Se completó el punto 3 de multitablas

--- Consultas/consultas_multitablas.sql
+++ Consultas/consultas_multitablas.sql
@@ -10,3 +10,12 @@ WHERE u.tipo_id = 1;
 -- 2
 SELECT nombre, precio_unitario FROM detalles_pedidos AS dp
 JOIN productos AS p ON dp.producto_id = p.producto_id
+
+-- 3
+SELECT 
+    u.nombre AS cliente,
+    e_usuario.nombre AS empleado
+FROM pedidos AS p
+JOIN usuarios AS u ON p.cliente_id = u.usuario_id
+JOIN empleados AS e ON p.empleado_id = e.empleado_id
+JOIN usuarios AS e_usuario ON e.usuario_id = e_usuario.usuario_id;
