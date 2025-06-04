f9ab0e4028c865a3ff0a326ef3287e7a135f486d
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-06-04 17:26:06 -0500

    feat: :sparkles: Se compeltó el punto 1 de subconsultas

---
+++ Consultas/subconsultas.sql
@@ -0,0 +1,9 @@
+-- Active: 1749062362566@@127.0.0.1@3307@taller_sql
+
+SHOW TABLES;
+
+-- 1
+SELECT u.nombre FROM usuarios AS u
+JOIN pedidos AS pe ON pe.cliente_id = u.usuario_id
+JOIN detalles_pedidos AS dp ON pe.pedido_id = dp.pedido_id
+WHERE dp.precio_unitario > 500000;
