914cb9edcab8f43651003b40a44652a34f58ecf6
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-06-04 17:26:34 -0500

    feat: :sparkles: Se completó el punto 2 de subconsultas

--- Consultas/subconsultas.sql
+++ Consultas/subconsultas.sql
@@ -7,3 +7,7 @@ SELECT u.nombre FROM usuarios AS u
 JOIN pedidos AS pe ON pe.cliente_id = u.usuario_id
 JOIN detalles_pedidos AS dp ON pe.pedido_id = dp.pedido_id
 WHERE dp.precio_unitario > 500000;
+
+-- 2
+SELECT * FROM pedidos
+WHERE estado = 'Cancelado';
