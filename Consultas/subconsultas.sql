0df99350a59cb227794615598605e68375d89ce3
JuanDavid888 <190215642+JuanDavid888@users.noreply.github.com>
2025-06-05 15:51:45 -0500

    feat: :sparkles: Se completó el punto 11 de subconsultas

--- Consultas/subconsultas.sql
+++ Consultas/subconsultas.sql
@@ -89,6 +89,17 @@ SELECT
     pe.fecha_pedido
 FROM pedidos pe
 JOIN usuarios u ON u.usuario_id = pe.cliente_id
-WHERE tipo_id = 1
+WHERE u.tipo_id = 1
 ORDER BY pe.fecha_pedido DESC
 LIMIT 1;
+
+-- 11
+SELECT
+    pe.pedido_id,
+    pr.producto_id,
+    pr.nombre,
+    pe.fecha_pedido
+FROM pedidos pe
+LEFT JOIN detalles_pedidos dp ON pe.pedido_id = dp.pedido_id
+LEFT JOIN productos pr ON pr.producto_id = dp.producto_id 
+WHERE pe.fecha_pedido >= CURDATE() - INTERVAL 3 MONTH
